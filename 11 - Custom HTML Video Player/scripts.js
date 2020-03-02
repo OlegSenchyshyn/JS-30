5ae2bdae38fa8d6db25e5a063b15a0acaef67d82
Oleh Senchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2020-03-02 17:27:58 +0200

    adding some comments

--- 11 - Custom HTML Video Player/scripts.js
+++ 11 - Custom HTML Video Player/scripts.js
@@ -1,4 +1,4 @@
-//Get videoents
+//Get our elements
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
@@ -25,6 +25,7 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
+//skip buttons
 function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
@@ -33,11 +34,14 @@ function handleRangeUpdate() {
   video[this.name] = this.value;
 }
 
+//progress bar
+//1.updating button in real time
 function handleProgress() {
   const percent = (video.currentTime / video.duration) * 100;
   progressBar.style.flexBasis = `${percent}%`;
 }
-
+//progress bar
+//2.when you click on it and drag it should update the video to correspond with that place
 function scrub(e) {
   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
   video.currentTime = scrubTime;
@@ -60,16 +64,24 @@ function openFullscreen() {
 }
 
 //Hook up the event listners
-video.addEventListener('click', togglePlay);
-video.addEventListener('play', updateButton);
-video.addEventListener('pause', updateButton);
-video.addEventListener('timeupdate', handleProgress);
-toggle.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay); //click the video to play/pause
+video.addEventListener('play', updateButton); //when the video is played run updateButton
+video.addEventListener('pause', updateButton); //when the video is paused run updateButton
+video.addEventListener('timeupdate', handleProgress);//listen for the video to emit an event called timeupdate and when that happens we run handleProgress function
+toggle.addEventListener('click', togglePlay); // click the button ► to play/pause
 skipButtons.forEach(button => button.addEventListener('click', skip));
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate)); //listen for a change event on each of those ranges
 
+//progress bar
 let mousedown = false;
-progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', e => mousedown && scrub(e));
+progress.addEventListener('click', scrub); //listen for a click -> when that happens run scrub function
+progress.addEventListener('mousemove', e => mousedown && scrub(e)); //first checks mouse down variable and if it's true, it moves to checking scrub
+/*Another way:
+progress.addEventListener('mousemove', () => {
+    if (mousedown) {
+        scrub();
+    }
+});
+*/
 progress.addEventListener('mousedown', () => (mousedown = true));
 progress.addEventListener('mouseup', () => (mousedown = false));
