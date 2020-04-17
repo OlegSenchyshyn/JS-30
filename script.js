a904288ed6bc4767eda92437b210185e504a3dd9
Oleh Senchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2020-04-17 21:57:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const divs = document.querySelectorAll('div'); //select all <div> elements
+const button = document.querySelector('button'); //selecting button
+
+function logText(e) {
+  console.log(this.classList.value);
+  //   e.stopPropagation(); // stop bubbling!! you'll see only 1 value: one, two or three
+  // console.log(this);
+}
+
+//when user clicks a div, we're going to log it
+divs.forEach((div) =>
+  div.addEventListener('click', logText, {
+    capture: false, //running from three to one
+    //capture: true, //running from one to three
+    once: true /* the same as 
+    div.removeEventListener('click', logText, {
+        capture: false,
+    });*/,
+  })
+);
+
+button.addEventListener(
+  'click',
+  () => {
+    console.log('Click!!!');
+  },
+  {
+    once: true, //listen for a click and then unbind itself so there's no future clicks on it
+  }
+);
